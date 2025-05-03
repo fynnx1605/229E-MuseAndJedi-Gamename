3d22b60b4687e0c9de2f7600e47d9760a3612d5c
fynnx1605 <nalinrat.ninr@bumail.net>
2025-05-03 17:18:31 +0700

    Joint2D_Bridge

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+        Rigidbody2D rb2d;
+        float move;
+        [SerializeField]float speed;
+        [SerializeField] float jumpforce;
+        [SerializeField] bool isJumping;
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+            
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            rb2d.AddForce(moveInput*speed);
+            /*move = Input.GetAxis("Horizontal");
+            rb2d.velocity = new Vector2(move * speed, rb2d.velocity.y);*/
+            if (Input.GetButtonDown("Jump") && !isJumping)
+            {
+                rb2d.AddForce(new Vector2(rb2d.velocity.x, jumpforce));
+                Debug.Log("Jump");
+            }
+        }
+    
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                isJumping = false;
+            }
+        }
+    
+    
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                isJumping = true;
+            }
+            
+            
+        }
+}
